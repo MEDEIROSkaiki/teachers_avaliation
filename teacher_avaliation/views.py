676347f478265b6499ddbbd11254d7ad76922ef8
letsrosa <leticiarosasouza6304@gmail.com>
2025-10-01 21:01:02 -0300

    ajustando login, logout e cadastro

--- teacher_avaliation/views.py
+++ teacher_avaliation/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
 @login_required
 def home(request):
     return render(request, 'teacher_avaliation/home.html')
 
-# Create your views here.
-def login(request):
-    return render(request, 'teacher_avaliation/login.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # loga automaticamente depois do cadastro
+            return redirect('home')    # redireciona para a home após cadastro
+    else:
+        form = UserCreationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'teacher_avaliation/signup.html', context)
